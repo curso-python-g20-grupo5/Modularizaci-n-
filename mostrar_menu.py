fb984db9b5f99dfe6b7eb108b3f27fbccef9c0b5
Rosa Rubio <paulina.rubio.p@gmail.com>
2024-08-14 21:20:39 -0400

    Se crea docstring para mostrar_menu.py

--- mostrar_menu.py
+++ mostrar_menu.py
@@ -1,4 +1,17 @@
 def mostrar_menu():
+    """
+    Muestra el menú de opciones para personalizar la pizza.
+
+    La función imprime un menú numerado que permite al usuario seleccionar 
+    diferentes opciones para personalizar su pizza. Las opciones incluyen cambiar 
+    la masa, cambiar la salsa, mostrar los ingredientes disponibles, modificar los 
+    ingredientes actuales, mostrar la configuración actual de la pizza, confirmar el pedido, 
+    y salir del programa.
+
+    Args:
+        None
+
+    """
     print("\n--- Menú de Opciones ---")
     print("1. Cambiar masa")
     print("2. Cambiar salsa")
