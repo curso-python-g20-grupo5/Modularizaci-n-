3294b0fd6b0876e661bee03eaa41e8fa5e4af7b2
Rosa Rubio <paulina.rubio.p@gmail.com>
2024-08-14 21:18:59 -0400

    Se crea docstring para mostrar_ingredientes_disponibles.py

--- mostrar_ingredientes_disponibles.py
+++ mostrar_ingredientes_disponibles.py
@@ -1,4 +1,16 @@
 def mostrar_ingredientes_disponibles(ingredientes_disponibles):
+    """
+    Muestra una lista de todos los ingredientes disponibles para añadir a la pizza.
+
+    La función imprime los ingredientes disponibles, numerados para facilitar la selección 
+    en otras partes del programa.
+
+    Args:
+        ingredientes_disponibles (list): Lista de ingredientes disponibles para agregar a la pizza.
+
+    Returns:
+        None
+    """
     print("\n--- Ingredientes Disponibles ---")
     for i, ingrediente in enumerate(ingredientes_disponibles, start=1):
         print(f"{i}. {ingrediente}")
